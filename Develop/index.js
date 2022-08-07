79f2c5e685b6f25bfb4e055d40f10aa04ff7c71b
hunter-williams <dragynson@yahoo.com>
2022-08-07 14:48:02 -0700

    html wireframe

---
+++ Develop/index.js
@@ -0,0 +1,16 @@
+// display current date
+moment().format("dddd, MMMM Do YYYY");
+// display timeblocks for standard buisness hours
+
+// time blocks are color coded 
+    // past 
+    // present
+    // future
+
+// onclick 
+
+    // enter an event
+    // save to local storage
+
+// refresh page
+    // pull from local storage
