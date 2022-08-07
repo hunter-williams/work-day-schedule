542237745d9daba97730256ced0758826076e6de
hunter-williams <dragynson@yahoo.com>
2022-08-07 15:48:21 -0700

    current date and color coding

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,30 @@
 // display current date
-moment().format("dddd, MMMM Do YYYY");
-// display timeblocks for standard buisness hours
+const today = new Date();
+var currentHour = moment(today).format('H')
+var day = moment().format('LLLL')
+
+document.getElementById('current-day').innerHTML = day
 
 // time blocks are color coded 
-    // past 
-    // present
-    // future
-
+var allTimes = $('.row-md-1')
+console.log(currentHour)
+console.log(allTimes.length)
+for (var i = 0; i < allTimes.length; i++){
+    if (allTimes[i].id < currentHour){
+       // past
+       console.log('past', allTimes[i].id)
+       allTimes[i].style.backgroundColor = 'red'  
+    } else if (allTimes[i].id === currentHour){
+        // present
+        console.log('present', allTimes[i].id)
+        allTimes[i].style.backgroundColor = 'yellow'  
+    } else if (allTimes[i].id > currentHour){
+         // future
+         console.log('future', allTimes[i].id)
+         allTimes[i].style.backgroundColor = 'green'  
+    }
+}
+    
 // onclick 
 
     // enter an event
