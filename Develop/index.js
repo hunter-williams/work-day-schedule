9f783cbf9feec47864fc9ff96f355ce270939624
hunter-williams <dragynson@yahoo.com>
2022-08-07 17:19:38 -0700

    save and check local storage

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,12 @@ const today = new Date();
 var currentHour = moment(today).format('H')
 var day = moment().format('LLLL')
 
-document.getElementById('current-day').innerHTML = day
+$('#current-day').html(day)
+
+// for (var i =0; i < localStorage.length; i++){
+//     const key = localStorage.key(i)
+//     console.log(`${key}:${localStorage.getItem(key)}`)
+// }
 
 // time blocks are color coded 
 var allTimes = $('.row-md-1')
@@ -13,22 +18,38 @@ for (var i = 0; i < allTimes.length; i++){
     if (allTimes[i].id < currentHour){
        // past
        console.log('past', allTimes[i].id)
-       allTimes[i].style.backgroundColor = 'red'  
+       allTimes[i].style.backgroundColor = 'red' 
+       checkStorage(allTimes[i].id) 
     } else if (allTimes[i].id === currentHour){
         // present
         console.log('present', allTimes[i].id)
-        allTimes[i].style.backgroundColor = 'yellow'  
+        allTimes[i].style.backgroundColor = 'yellow'
+        checkStorage(allTimes[i].id)   
     } else if (allTimes[i].id > currentHour){
          // future
          console.log('future', allTimes[i].id)
-         allTimes[i].style.backgroundColor = 'green'  
+         allTimes[i].style.backgroundColor = 'green' 
+         checkStorage(allTimes[i].id)  
     }
 }
-    
-// onclick 
 
+// onclick 
+function saveNote(saveButton){
+    var row = saveButton.parentElement
+    var input = row.children[1].value
+    var id = row.id
+    console.log('Id:',id,'Input: ',input)
+    
+    window.localStorage.setItem(id,input)
+    var get = localStorage.getItem(id)
+    console.log(get)
+}
     // enter an event
     // save to local storage
-
+function checkStorage(id){
+    if (localStorage.getItem(id)){
+        document.getElementById(id).children[1].value = localStorage.getItem(id)
+    }
+}
 // refresh page
     // pull from local storage
